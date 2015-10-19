77e5add812dc96a73d1d85bd13b406c5593b3460
quakkels <quakkels@gmail.com>
2015-10-18 21:12:37 -0500

    renamed config package to images

--- goshipit.go
+++ goshipit.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"github.com/aphistic/gomol"
-	"github.com/quakkels/goshipit/config"
+	"github.com/quakkels/goshipit/images"
 )
-
-var configs config.Images
 
 func main() {
 	startUp()
 	defer cleanUp()
 
-	configs, err := config.NewImages("config.json")
+	images, err := images.NewImages("config.json")
 	if err != nil {
 		gomol.Fatal(err.Error())
 		panic(err)
 	}
 
-	gomol.Infof("Configs: %v", configs)
+	gomol.Infof("images: %v", images)
 }
 
 func startUp() {
