709d276dd4f68269b16f3143c1c435853bf7ab76
quakkels <quakkels@gmail.com>
2015-10-27 08:32:17 -0500

    Working out testing for slashcommandsmodel.

--- slack/slash_command_model_test.go
+++ slack/slash_command_model_test.go
@@ -1,21 +1,36 @@
 package slack
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 	"testing"
 )
 
 func TestNewSlashCommandModel(t *testing.T) {
 	// arrange
-	myReq, _ := http.NewRequest("POST", "Idontcare", strings.NewReader("token=testtokenvalue"))
+	values := url.Values{}
+	values.Add("token", "testtokenvalue")
+	myReq, err := http.NewRequest(
+		"POST",
+		"http://example.com",
+		strings.NewReader(values.Encode()))
+	readAll, _ := ioutil.ReadAll(myReq.Body)
 
 	// act
 	myModel := NewSlashCommandModel(myReq)
 
 	// assert
+	if err != nil {
+		t.Log(err.Error())
+	}
+
 	if myModel.Token != "testtokenvalue" {
 		t.Log("Wrong value in Token field.")
+		t.Log("myModel.Token: " + myModel.Token)
+		t.Log("myReq.FormValue: " + myReq.FormValue("token"))
+		t.Log("ioutil: " + string(readAll))
 		t.Fail()
 	}
 }
